b7768a946c064ab2e7c945f545c07df2afb2eff3
Anthony Candelino <anthcandelino@gmail.com>
2024-09-22 14:48:38 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Anthony Candelino on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
